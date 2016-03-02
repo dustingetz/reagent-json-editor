e033465b520a75729bdeb623ca45e0a3921f30e4
Karl Hardenstine <khardenstine@gmail.com>
2016-03-02 11:58:29 -0500

    Build out example framework

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -3,25 +3,40 @@
             [reagent-json-editor.core :as foo]
             [cursor.core :refer [cursor]]))
 
-(def store
-  (reagent/atom
-    {:a 10
-     :b "20"
-     :c nil
-     :d {:foo {:bar  42
-               :baz  55
-               :buzz "womp"}}
-     :e [{:name "Alice" :id 0}
-         {:name "Bob" :id 1}
-         {:name "Charlie" :id 2}
-         {:name "David" :id 3}]}))
+(defonce store
+  (reagent/atom {}))
 
 
-(def rootCursor (cursor store))
+(defn render [k store old-val new-val]
+      (let [cur (cursor store)]
+           (reagent/render-component
+             [:div.App
+              [:div
+               "State"
+               [:pre (js/JSON.stringify (clj->js @cur) nil 2)]]
+              [:div
+               "Editor"]
+              [:div "Second editor, because we can"]
+
+              ]
+             (.getElementById js/document "root"))))
+
+(def initial-state {:a 10
+                    :b "20"
+                    :c nil
+                    :d {:foo {:bar  42
+                              :baz  55
+                              :buzz "womp"}}
+                    :e [{:name "Alice" :id 0}
+                        {:name "Bob" :id 1}
+                        {:name "Charlie" :id 2}
+                        {:name "David" :id 3}]})
+
+(defn main []
+      (add-watch store :react-render render)
+      (reset! store initial-state)
+      )
+
 
 (defn idempotent-render []
-  (reagent/render-component
-    [:div.App "hello"
-     "State"
-     [:pre (js/JSON.stringify (clj->js @store) nil 2)]]
-    (.getElementById js/document "root")))
+      (render :react-render store @store @store))
