6eab4f7b4d8414f4df5d451e9c16c6c3d49e82cb
Karl Hardenstine <khardenstine@gmail.com>
2016-03-02 17:50:21 -0500

    Finish LeafEditor logic

--- vendor/reagent-json-editor/src/reagent_json_editor/JsonLeafEditor.cljs
+++ vendor/reagent-json-editor/src/reagent_json_editor/JsonLeafEditor.cljs
@@ -10,7 +10,8 @@
       (js/console.log "editing true"))
 
 (defn commit [cur state]
-      (swap! cur (constantly (js/JSON.parse (:jsValue @state)))))
+      (swap! cur (constantly (js/JSON.parse (:jsValue @state))))
+      (swap! state update-in [:editing] (constantly false)))
 
 (defn isValid [value]
   (try
@@ -21,24 +22,23 @@
       false)))
 
 (defn isDirty [cur state]
-      false;  (!= @cur (:jsValue @state))
-      )
+      (not= (js/JSON.stringify (clj->js @cur)) (:jsValue @state)))
 
 (defn JsonLeafEditor [cur]
       (let [state (reagent/atom {
                                  :jsValue (js/JSON.stringify (clj->js @cur) nil 2)
                                  :editing false
                                  })]
-           (fn [cur] [:span.JsonLeafEditor.dirty.invalid
-                      (if (:editing @state)
-                        [:div
-                         [:input {:key      "foo"
-                                  :value    (:jsValue @state)
-                                  :onChange #(onChange state %)
-                                  :style    {:background "transparent"}}]
-                         [:button {:key      "bar"
-                                   :onClick  #(commit cur state)
-                                   :disabled (not (isValid (:jsValue @state)))}
-                          "commit"]]
-                        [:code.editButton {:onClick #(edit! state)}
-                          (:jsValue @state)])])))
+           (fn [cur]
+               [:span {:class (str "JsonLeafEditor" (if (isDirty cur state) " dirty" nil) (if (isValid (:jsValue @state)) nil " invalid"))}
+                (if (:editing @state)
+                  [:div
+                    [:input {:key      "foo"
+                             :value    (:jsValue @state)
+                             :onChange #(onChange state %)
+                             :style    {:background "transparent"}}]
+                   [:button {:key      "bar"
+                             :onClick  #(commit cur state)
+                             :disabled (not (isValid (:jsValue @state)))}
+                    "commit"]]
+                  [:code.editButton {:onClick #(edit! state)} @cur])])))
