973ef13d680742ecf537993d98e5486cf75d263f
Karl Hardenstine <khardenstine@gmail.com>
2016-03-23 12:55:22 -0400

    Readd provided dependency on react-treeview

--- vendor/reagent-json-editor/src/reagent_json_editor/core.cljs
+++ vendor/reagent-json-editor/src/reagent_json_editor/core.cljs
@@ -1,6 +1,6 @@
 (ns reagent-json-editor.core
   (:require [reagent.core :as reagent]
-            #_ [react-treeview]
+            [react-treeview]
             [reagent-json-editor.JsonLeafEditor :refer [JsonLeafEditor]]))
 
 (declare JsonEditor render-node)
